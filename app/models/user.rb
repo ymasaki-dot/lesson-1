df619a901d2d7e7e82a4d055e33524d9f878851a
ymasaki-dot <y.masaki0526@gmail.com>
2021-04-08 06:12:41 +0000

    update[フォロー機能実装]

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,30 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
-  
   has_many :book_comment, dependent: :destroy
   has_many :favorites, dependent: :destroy
-
-
+  
+  #自分がフォローする（与フォロー）
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  #自分がフォローされる（被フォロー）
+  has_many :revers_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 被フォロー関係を通じて参照→自分をフォローしている人
+  has_many :followers, through: :revers_of_relationships, source: :follower
+  # 与フォロー関係を通じて参照→自分がフォローしている人
+  has_many :followings, through: :relationships, source: :followed
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
-  resources :users,only: [:show,:index,:edit,:update]
-  
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships,only: [:create,:destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+end
   resources :books,only: [:index, :show, :edit, :create, :destroy, :update] do
     resource :favorites,only: [:create, :destroy]
     resources :book_comments,only: [:create, :destroy]
