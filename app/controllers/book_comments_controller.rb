9a13da3bb652f1e4c03fe69a881ec6e0d2d12891
ymasaki-dot <y.masaki0526@gmail.com>
2021-04-09 09:36:36 +0000

    update[コメント、いいね機能非同期]

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,10 +6,8 @@ class BookCommentsController < ApplicationController
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
 		@book_comment.user_id = current_user.id
-		if @book_comment.save
-  		redirect_to book_path(@book.id)
-		else
-		  render 'books/show'
+		unless @book_comment.save
+		  render 'error'
 		end
 	end
 
@@ -17,7 +15,6 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
   		book_comment = @book.book_comments.find(params[:id])
 		book_comment.destroy
-		redirect_to request.referer
 	end
 
 	private
